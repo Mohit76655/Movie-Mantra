55fd29929a8426e93de1f985022d40d2a93c2876
Mohit76655 <mohit76655@gmail.com>
2024-01-27 23:55:18 +0530

    Api test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
 
 function App() {
+  useEffect(() => {
+    apiTest();
+  }, []);
+
+  const apiTest = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+    });
+  };
+
   return (
     <>
       <div className="App"></div>
