dbcc3e62b090b7bd0efea89b74ef3768b2f6f35f
Mohit76655 <mohit76655@gmail.com>
2024-02-21 00:18:01 +0530

    bug fix

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./home.scss";
 import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending/Trending";
 
 const Home = () => {
   return (
     <div className="">
       <HeroBanner />
+      <Trending />
     </div>
   );
 };
